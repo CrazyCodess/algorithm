2b28216d47b459cc8f6bb1f2fa06702553b7d4c1
CrazyCodess <1312480794@qq.com>
2016-05-28 13:39:01 +0800

    使用sublime text3编写C++

---
+++ 计蒜客/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main(){
+	int a;
+	a=112;
+	printf("%d\n", a);
+	return 0;
+} 
---
+++ 计蒜客/数正方形.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int dp[251][251];
+char s[251][251];
+int main(){
+	int n;
+
+}
