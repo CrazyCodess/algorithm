1586d644c71cf2be2a6fe7b540fcfe828865318a
CrazyCodess <1312480794@qq.com>
2016-05-31 00:59:58 +0800

    求n*n数组中的和最大的矩形

--- poj/1050.cpp
+++ poj/1050.cpp
@@ -14,7 +14,26 @@ int main(){
 				scanf("%d",&num[i][j]);
 			}
 		}
-
+		int max=0,sum;
+		for ( i = 0; i < n; ++i)
+		{
+			for ( j = i; j < n; ++j)
+			{
+				sum=0;
+				for (int k = 0; k < n; ++k)
+				{
+					int temp=0;
+					for (int m = i; m <= j; ++m)
+					{
+						temp+=num[k][m];	//竖着加某一列
+					}
+					sum+=temp;
+					if(sum<0)sum=0;			//当加到小于的时候置为0，重新开始加。
+					else if(sum>max)max=sum;
+				}
+			}
+		}
+		printf("%d\n", max);
 
 
 	}
