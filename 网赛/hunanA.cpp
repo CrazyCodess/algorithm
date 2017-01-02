c4a253bb6aba19ee2a043bff66b1a8e7768726ed
CrazyCodess <1312480794@qq.com>
2017-01-03 01:48:03 +0800

    17/1/3

---
+++ 网赛/hunanA.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*int solve(int n){
+	int cnt=0;
+	for(int i=1;i<=n;i++){
+		if(n%i==0){
+			printf("%d ",i);
+			cnt++;
+		}
+	}
+	return cnt;
+}*/
+int main(){
+	long long  n,m,t,cnt;
+	int i,j;
+	while(scanf("%lld%lld",&n,&m)){
+		if(m<n){
+			t=n;
+			n=m;
+			m=t;
+		}
+		if(n>=2016){
+			
+		}
+		for(i=1;i<=n;i++){
+
+		}
+
+
+	}
+		cout<<solve(2016)<<endl;
+	return 0;
+}
